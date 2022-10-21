0c3be4b93a91fe25583102e3d8933463e3a3e1d3
Maicon Mota <maiconlmotadev@hotmail.com>
2022-10-21 18:27:52 +0100

    Upadate: Components, images and others

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pm-root',
   template: `
-    <div><h1>{{pageTitle}}</h1>
-        <app-products>Component</app-products>
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+      <a class='navbar-brand'>{{pageTitle}}</a>
+      <ul class='nav nav-pills'>
+        <li><a class='nav-link' routerLink='/welcome'>Home</a></li>
+        <li><a class='nav-link' routerLink='/products'>Lista de Produtos</a></li>
+      </ul>
+    </nav>
+    <div class='container'>
+      <router-outlet></router-outlet>
     </div>
   `
+
 })
 export class AppComponent {
-  pageTitle: string = 'CesaeDigital LOJA';
+  pageTitle: string = 'Cesae Digital STORE';
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,17 +11,24 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule}  from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 
+import {ProductListComponent} from "./products/product-list.component";
 
-import {ProductListComponent} from "./product/product-list.component";
-import {FormsModule} from "@angular/forms";
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-
+import {StarComponent} from "./shared/star.component";
+import { ProductDetailComponent } from './products/product-detail.component';
+import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    ProductDetailComponent
+
 
   ],
   imports: [
@@ -30,6 +39,14 @@ import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
     BrowserAnimationsModule,
     MatButtonModule,
     FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: "Welcome", pathMatch: "full" },
+      { path: '++', redirectTo: "Welcome", pathMatch: "full" },
+    ]),
 
   ],
   providers: [],
